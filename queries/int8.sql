a0d1f373d088d4fddd45e0c252cffaf03a5e7663
Valter Castro <valterochoacastro@gmail.com>
2022-12-21 14:46:34 +0000

    Created int8

--- queries/int8.sql
+++ queries/int8.sql
@@ -1,1 +1,16 @@
--- Em que minuto foi o último golo da equipa que marcou mais golos?
+-- Qual o golo mais tardio da equipa que marcou mais golos?
+
+
+SELECT EquipaComMaisGolos, NumeroDeGolos, a.tempo AS 'GoloMaisTardio'
+FROM 
+(SELECT Equipa.nome AS 'EquipaComMaisGolos', COUNT(*) AS 'NumeroDeGolos', Evento.ID AS 'golosa', Evento.tempo AS 'tempo'
+FROM Equipa, Evento, Golo, Jogador
+WHERE Evento.ID = Golo.eventoID
+AND Jogador.pessoaID = Golo.jogadorID
+AND Jogador.equipaID = Equipa.ID
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1) as a, Golo
+WHERE a.golosa = Golo.eventoID
+ORDER BY 3 DESC
+LIMIT 1;
