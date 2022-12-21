3de4df8108bda8e19e258b385d6d5b646957ae84
Valter Castro <valterochoacastro@gmail.com>
2022-12-21 20:10:18 +0000

    Changed int7

--- queries/int7.sql
+++ queries/int7.sql
@@ -1,14 +1,17 @@
---Qual o jogador espanhol com mais golos no campeonato?
+--Media de golos marcados por jogador?
 
 .mode columns
 .headers on
 .nullvalue NULL
 
-SELECT DISTINCT Pessoa.nome AS 'Nome', COUNT(*) AS 'Nº de Golos'
-FROM Jogador, Pessoa, Evento, Golo
-WHERE Jogador.pessoaID = Pessoa.ID 
-AND Golo.eventoID = Evento.ID 
+
+SELECT ROUND(AVG(Golos),2) AS 'Media de golos marcados por jogador'
+FROM(
+SELECT Pessoa.nome AS 'Jogador', COUNT(*) AS 'Golos'
+FROM Jogador, Pessoa, Golo, Evento
+WHERE Jogador.pessoaID = Pessoa.ID
+AND Golo.eventoID = Evento.ID
 AND Golo.jogadorID = Jogador.pessoaID
-AND Jogador.nacionalidade = "espanhola"
 GROUP BY 1
-limit 1;
+ORDER BY 2 DESC
+)
